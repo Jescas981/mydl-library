794bf19ff87d21fa5813d75e183b36241a875876
Jescas981 <jescas981@gmail.com>
2024-04-06 02:21:47 -0500

    Add device to one hot

--- mydllib/functional/one_hot.py
+++ mydllib/functional/one_hot.py
@@ -2,6 +2,6 @@ import torch
 
 
 def one_hot(y: torch.Tensor, n_features: int) -> torch.Tensor:
-    output = torch.zeros(y.shape[0], n_features)
+    output = torch.zeros(y.shape[0], n_features, device=y.device)
     output[torch.arange(y.shape[0]), y] = 1
     return output
